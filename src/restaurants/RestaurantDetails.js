e653d8ce5ffe4844ca32f18e1b600c5ecbd504b3
Elizabeth Nguyen <elizabethtallentnguyen@gmail.com>
2025-07-26 17:15:41 -0400

    Card to grid in details

--- src/restaurants/RestaurantDetails.js
+++ src/restaurants/RestaurantDetails.js
@@ -1,21 +1,37 @@
-import { Card, CardText, CardBody, Col } from 'reactstrap';
+import { Card, CardText, CardBody } from 'reactstrap';
 
 const RestaurantDetail = ({ restaurant }) => {
     const { name, rating, type, category } = restaurant;
 
     return (
-        <Col md="4" className="mb-4">
-            <Card className="bg-light border p-4">
-                <CardBody>
-                    <h4>{name}</h4>
-                    <CardText>{rating}</CardText>
-                    <CardText>{type}</CardText>
-                    <CardText>{category}</CardText>
-                </CardBody>
-            </Card>
-        </Col>
-
+        <>
+            <div className="min-vh-100 d-flex justify-content-center align-items-center bg-light">
+                <div class="container text-center">
+                <div class="row">
+                    <div class="col">
+                        <h4>{name}</h4>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p>{rating}</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p>{type}</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p>{category}</p>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </>
     )
 };
+
 
 export default RestaurantDetail;
